9d20e8e7187f4a4c1ec684c3f4f145d43fca88e6
Javiera Alvarez <149703653+JavieraFAC@users.noreply.github.com>
2024-10-23 18:13:49 -0300

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,7 @@
 // navbar.js
 function loadNavbar() {
     const navbarHTML = `
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark" style="padding-top:30px; padding-left:20px; padding-right:20px">
         <div class="container-fluid">
             <a class="navbar-brand" href="index.html">QUILTRO</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
